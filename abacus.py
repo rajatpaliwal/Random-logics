00c8c40b9227e5ccc2350cba8aa84e86c00d228e
rajatpaliwal <30369775+rajatpaliwal@users.noreply.github.com>
2017-07-28 01:19:53 +0530

    Update abacus.py

--- abacus.py
+++ abacus.py
@@ -1,3 +1,41 @@
+
+# Random-logics
+#########################################################################
+#                 10-row School abacus
+#                         by
+#                      Michael H
+#########################################################################
+#       Description partially extracted from from wikipedia 
+#
+#  Around the world, abaci have been used in pre-schools and elementary
+#
+# In Western countries, a bead frame similar to the Russian abacus but
+# with straight wires and a vertical frame has been common (see image).
+# Helps schools as an aid in teaching the numeral system and arithmetic
+#
+#         |00000*****   |     row factor 1000000000
+#         |00000*****   |     row factor 100000000
+#         |00000*****   |     row factor 10000000 
+#         |00000*****   |     row factor 1000000
+#         |00000*****   |     row factor 100000
+#         |00000*****   |     row factor 10000
+#         |00000*****   |     row factor 1000
+#         |00000****   *|     row factor 100     * 1
+#         |00000***   **|     row factor 10      * 2
+#         |00000**   ***|     row factor 1       * 3
+#                                        -----------    
+#                             Sum                123 
+#
+# Each row represents a different row factor, starting with x1 at the
+# bottom, ascending up to x1000000000 at the top row.     
+######################################################################
+
+# TASK:
+# Define a procedure print_abacus(integer) that takes a positive integer
+# and prints a visual representation (image) of an abacus setup for a 
+# given positive integer value.
+# 
+
 def print_abacus(value):
     a,b,c,d,e,f,g,h,i,j='|00000*****   |','|00000****   *|','|00000***   **|','|00000**   ***|','|00000*   ****|','|00000   *****|','|0000   0*****|','|000   00*****|','|00   000*****|','|0   0000*****|'
     p=[a,b,c,d,e,f,g,h,i,j]
